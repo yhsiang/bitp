4bb4abfcc04f95f838f93c90e07c0ee4924fb9f4
LY Cheng <yhsiang@maicoin.com>
2019-11-04 12:57:35 +0800

    add history

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
+const cheerio = require('cheerio');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
@@ -9,6 +10,12 @@ app.use('/bitoex', function(req, res) {
   const url = 'https://www.bitoex.com/sync/dashboard'
 
   req.pipe(request({ uri: url + '/' + Date.now() })).pipe(res);
+});
+
+app.use('/history/bitoex/:coin', function(req, res) {
+  const url = 'https://www.bitoex.com/charts/price_history';
+
+  req.pipe(request({ uri: url, qs: { currency: req.params.coin.toUpperCase() } })).pipe(res);
 });
 
 app.use('/maicoin/:coin', function(req, res) {
@@ -23,6 +30,52 @@ app.use('/maicoin/:coin', function(req, res) {
 
   req.pipe(request({ uri: url.replace('target', req.params.coin) }))
     .pipe(res);
+});
+
+function padZero(str) {
+  return ('0' + str).slice(-2);
+}
+
+function oneYearAgo() {
+  const now = new Date();
+  const year = now.getFullYear() - 1;
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const hours = now.getHours();
+  const mins = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  return `${year}-${padZero(month)}-${padZero(day)} ${padZero(hours)}:${padZero(mins)}:${padZero(seconds)} +0800`;
+}
+
+app.use('/history/maicoin/:coin', function(req, res) {
+  const url = 'https://www.maicoin.com/en/trade_summaries';
+
+  request('https://www.maicoin.com/en/charts', (error, response, body) => {
+    const $ = cheerio.load(body);
+    const cookie = response.headers['set-cookie'].map(it => {
+      return it.split(';')[0]
+    }).filter(it => it.startsWith('_twcoin_session_production')).join(';');
+    const csrfToken = $('meta[name="csrf-token"]').attr('content');
+
+    request({
+      url: `${url}/${req.params.coin.toLowerCase()}`,
+      method: 'POST',
+      qs: {
+        from: oneYearAgo(),
+      },
+      headers: {
+      'cookie': cookie,
+      'x-csrf-token': csrfToken,
+      'x-requested-with': 'XMLHttpRequest',
+      },
+    }, function(err, response2, body) {
+      const str = body.split('data')[1];
+      const data = str.split('xkey')[0];
+      const d = data.replace(': ', '').replace(',\n', '');
+      res.json(JSON.parse(d));
+    });
+  });
 });
 
 //Start the server by listening on a port
