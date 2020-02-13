885bc18484d9d1c6807e8a6dd7af53f49be9fc25
LY Cheng <yhsiang@maicoin.com>
2020-02-13 15:01:24 +0800

    use redis for cache

--- server.js
+++ server.js
@@ -1,80 +1,25 @@
 const express = require('express');
-const request = require('request');
 const cors = require('cors');
-const cheerio = require('cheerio');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(require('express-redis')(6379, '127.0.0.1', {
+  auth_pass: process.env.REDIS_PASSWORD,
+}));
+
 app.use(cors());
 
-app.use('/bitoex', function(req, res) {
-  const url = 'https://www.bitoex.com/sync/dashboard'
-
-  req.pipe(request({ uri: url + '/' + Date.now() })).pipe(res);
+app.get('/', function(req, res) {
+  res.json({ 'msg': 'it works!' });
 });
 
-app.use('/history/bitoex/:coin', function(req, res) {
-  const url = 'https://www.bitoex.com/charts/price_history';
+app.get('/price', function(req, res) {
+  req.db.get('price', function (err, reply) {
+    if (err) {
+      return res.status(500).end();
+    }
 
-  req.pipe(request({ uri: url, qs: { currency: req.params.coin.toUpperCase() } })).pipe(res);
-});
-
-app.use('/maicoin/:coin', function(req, res) {
-  const url = 'https://www.maicoin.com/api/prices/target-usd';
-
-  if (
-    req.params.coin !== 'btc' &&
-    req.params.coin !== 'eth' &&
-    req.params.coin !== 'ltc' &&
-    req.params.coin !== 'usdt'
-  ) return res.json({ msg: 'not found '});
-
-  req.pipe(request({ uri: url.replace('target', req.params.coin) }))
-    .pipe(res);
-});
-
-function padZero(str) {
-  return ('0' + str).slice(-2);
-}
-
-function oneYearAgo() {
-  const now = new Date();
-  const year = now.getFullYear() - 1;
-  const month = now.getMonth() + 1;
-  const day = now.getDate();
-  const hours = now.getHours();
-  const mins = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  return `${year}-${padZero(month)}-${padZero(day)} ${padZero(hours)}:${padZero(mins)}:${padZero(seconds)} +0800`;
-}
-
-app.use('/history/maicoin/:coin', function(req, res) {
-  const url = 'https://www.maicoin.com/en/trade_summaries';
-
-  request('https://www.maicoin.com/en/charts', (error, response, body) => {
-    const $ = cheerio.load(body);
-    const cookie = response.headers['set-cookie'].map(it => {
-      return it.split(';')[0]
-    }).filter(it => it.startsWith('_twcoin_session_production')).join(';');
-    const csrfToken = $('meta[name="csrf-token"]').attr('content');
-
-    request({
-      url: `${url}/${req.params.coin.toLowerCase()}`,
-      method: 'POST',
-      qs: {
-        from: oneYearAgo(),
-      },
-      headers: {
-      'cookie': cookie,
-      'x-csrf-token': csrfToken,
-      'x-requested-with': 'XMLHttpRequest',
-      },
-    }, function(err, response2, body) {
-      const str = body.split('data')[1];
-      const data = str.split('xkey')[0];
-      const d = data.replace(': ', '').replace(',\n', '');
-      res.json(JSON.parse(d));
-    });
+    res.json(JSON.parse(reply));
   });
 });
 
---
+++ worker.js
@@ -0,0 +1,40 @@
+const redis = require("redis");
+const request = require('request-promise-native');
+const client = redis.createClient();
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+const getBitoExPrice = async () => {
+  const uri = `https://www.bitoex.com/sync/dashboard/${Date.now()}`;
+  const body = await request({ uri });
+  return body;
+};
+
+const getMaicoinPrice = async (crypto) => {
+  const url = 'https://www.maicoin.com/api/prices/target-usd';
+  const body = await request({ uri: url.replace('target', crypto) });
+  return body;
+};
+
+Promise.all([
+  getBitoExPrice(),
+  getMaicoinPrice('btc'),
+  getMaicoinPrice('eth'),
+  getMaicoinPrice('ltc'),
+  getMaicoinPrice('usdt'),
+]).then(([bitoex, BTC, ETH, LTC, USDT]) => {
+    const maicoin = {
+      t: Date.now(),
+      BTC: JSON.parse(BTC),
+      ETH: JSON.parse(ETH),
+      LTC: JSON.parse(LTC),
+      USDT: JSON.parse(USDT),
+    };
+    client.set('price', JSON.stringify({
+      bitoex: JSON.parse(bitoex),
+      maicoin,
+    }));
+    client.quit();
+  });
