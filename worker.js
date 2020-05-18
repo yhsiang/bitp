a2d78df00d5ce452c8a4a8c107200110548ec075
LY Cheng <lyforever62@hotmail.com>
2020-05-18 14:44:52 +0800

    use axios and graphql

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const request = require('request-promise-native');
+const axios = require('axios');
 const client = redis.createClient();
 
 if (process.env.REDIS_PASSWORD) {
@@ -11,33 +11,60 @@ client.on("error", function(error) {
 });
 
 const getBitoExPrice = async () => {
-  const uri = `https://www.bitoex.com/api/v0/rate/${Date.now()}`;
-  const body = await request({ uri });
+  const url = `https://www.bitoex.com/api/v0/rate/${Date.now()}`;
+  const body = await axios.get(url);
   return body;
 };
 
 const getMaicoinPrice = async (crypto) => {
-  const url = 'https://www.maicoin.com/api/prices/target-usd';
-  const body = await request({ uri: url.replace('target', crypto) });
+  const query = `
+    query Ticker_GetTicker($currency: CryptoCurrency!) {
+      ticker(currency: $currency) {
+          buyPrice
+          sellPrice
+          __typename
+      }
+    }`;
+  const payload = {
+    operationName: 'Ticker_GetTicker',
+    query: query,
+    variables: {
+      currency: crypto,
+    },
+  };
+  const url = 'https://api.maicoin.com/graphql';
+  const body = await axios.post(url, payload);
   return body;
 };
 
 Promise.all([
   getBitoExPrice(),
-  getMaicoinPrice('btc'),
-  getMaicoinPrice('eth'),
-  getMaicoinPrice('ltc'),
-  getMaicoinPrice('usdt'),
+  getMaicoinPrice('BTC'),
+  getMaicoinPrice('ETH'),
+  getMaicoinPrice('LTC'),
+  getMaicoinPrice('USDT'),
 ]).then(([bitoex, BTC, ETH, LTC, USDT]) => {
     const maicoin = {
       t: Date.now(),
-      BTC: JSON.parse(BTC),
-      ETH: JSON.parse(ETH),
-      LTC: JSON.parse(LTC),
-      USDT: JSON.parse(USDT),
+      BTC: {
+        formatted_buy_price_in_twd: BTC.data.data.ticker.buyPrice,
+        formatted_sell_price_in_twd: BTC.data.data.ticker.sellPrice,
+      },
+      ETH: {
+        formatted_buy_price_in_twd: ETH.data.data.ticker.buyPrice,
+        formatted_sell_price_in_twd: ETH.data.data.ticker.sellPrice,
+      },
+      LTC: {
+        formatted_buy_price_in_twd: LTC.data.data.ticker.buyPrice,
+        formatted_sell_price_in_twd: LTC.data.data.ticker.sellPrice,
+      },
+      USDT: {
+        formatted_buy_price_in_twd: USDT.data.data.ticker.buyPrice,
+        formatted_sell_price_in_twd: USDT.data.data.ticker.sellPrice,
+      },
     };
     client.set('price', JSON.stringify({
-      bitoex: JSON.parse(bitoex),
+      bitoex: bitoex.data,
       maicoin,
     }));
     client.quit();
